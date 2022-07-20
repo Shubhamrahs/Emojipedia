a463fced6e4d94055b37c42a27d4818b4f9e581c
Shubhamrahs <19ucs038@lnmiit.ac.in>
2022-07-21 03:02:32 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,456 @@
+const emojipedia = [
+
+    {
+        id: 1,
+        emoji: "😀",
+        name: "Grinning",
+        meaning:"It conveys cheerfulness and joy towards something positive"
+    },
+    {
+        id: 2,
+        emoji: "😃",
+        name: "Smiley",
+        meaning: "Smiley emoji denotes happiness and positive feelings"
+    },
+    {
+        id: 3,
+        emoji: "😄",
+        name: "Smile",
+        meaning: "The smile emoji depicts a person’s happiness when sending a greeting text or simply used to compliment something or the other "
+    },
+    {
+        id: 4,
+        emoji: "😁",
+        name: "Grin",
+        meaning: "The grin emoji represents mischievousness but can be used to give away excitement and enjoyment"
+    },
+    {
+        id: 5,
+        emoji: "😆",
+        name: "Laughing",
+        meaning: "The emoji depicts laughter often used to react towards something hilarious"
+    },
+    {
+        id: 6,
+        emoji: "😅",
+        name: "Sweat smile",
+        meaning: "Used to depict a close call but is also often used in awkward situations during a chat"
+    },
+    {
+        id: 7,
+        emoji: "🤣",
+        name: "Rolling on the floor laughing",
+        meaning: "Used when someone cracks a hilarious joke"
+    },
+    {
+        id: 8,
+        emoji: "😂",
+        name: "Joy",
+        meaning: "Shedding tears because of laughing so hard"
+    },
+    {
+        id: 9,
+        emoji: "🙂",
+        name: "Slightly smiling face",
+        meaning: "An emoji which can have two or more meanings to it such as being positive, happy or patronizing and being ironic"
+    },
+    {
+        id: 10,
+        emoji: "🙃",
+        name: "Upside down face",
+        meaning: "The emoji depicts being silly and sarcastic"
+    },
+    {
+        id: 11,
+        emoji: "😉",
+        name: "Wink",
+        meaning: "This emoji signals a joke or a hidden meaning which when used won’t be understood by many"
+    },
+    {
+        id: 12,
+        emoji: "😊",
+        name: "Blush",
+        meaning: "This emoji expresses extreme happiness and positive feelings"
+    },
+    {
+        id: 13,
+        emoji: "😇",
+        name: "Innocent",
+        meaning: "This emoji means being as innocent and pure as an angel"
+    },
+    {
+        id: 14,
+        emoji: "😍",
+        name: "Heart eyes",
+        meaning: "Taken straight from cartoons, the hearted eyes emoji is used to convey feeling of love and attraction"
+    },
+    {
+        id: 15,
+        emoji: "😘",
+        name: "Kissing heart",
+        meaning: "The emoji is used to convey a goodbye to a very close one"
+    },
+    {
+        id: 16,
+        emoji: "😚",
+        name: "Kissing closed eyes",
+        meaning: "The pink cheeks and puckered lips on this emoji convey sentiments of love and affection"
+    },
+    {
+        id: 17,
+        emoji: "😙",
+        name: "Kissing smiling eyes",
+        meaning: "This emoji is also used to convey sentiments of love and affection but in a lighter way"
+    },
+    {
+        id: 18,
+        emoji: "😋",
+        name: "Yum",
+        meaning: "Used when one is about to have a delicious meal, or already had one"
+    },
+    {
+        id: 19,
+        emoji: "😛",
+        name: "Stuck out tongue",
+        meaning: "This conveys a sense of excitement, fun and cuteness"
+    },
+    {
+        id: 20,
+        emoji: "😜",
+        name: "Stuck out tongue winking eye",
+        meaning: "Used when just casually kidding around with jokes and funny conversations"
+    },
+    {
+        id: 21,
+        emoji: "😐️",
+        name: "Neutral face",
+        meaning: "When the other person doesn’t get the joke but can also be used in context to concern for someone"
+    },
+    {
+        id: 22,
+        emoji: "😑",
+        name: "Expressionless",
+        meaning: "The flat eyes and mouth convey a sense of frustration or disappointment"
+    },
+    {
+        id: 23,
+        emoji: "🤔",
+        name: "Thinking face",
+        meaning: "Depicts going into a deep state of thinking"
+    },
+    {
+        id: 24,
+        emoji: "🤫",
+        name: "Shushing face",
+        meaning: "When the chat has gone with loads of messages and you want to make everyone quiet"
+    },
+    {
+        id: 25,
+        emoji: "🤗",
+        name: "Hugging face",
+        meaning: "This emoji depicts a sense of affection for someone"
+    },
+    {
+        id: 26,
+        emoji: "🤐",
+        name: "Zipper mouth face",
+        meaning: "Used when secrets and promises are to be kept among only certain number of people"
+    },
+    {
+        id: 27,
+        emoji: "🤑",
+        name: "Money mouth face",
+        meaning: "This emoji conveys the feeling of getting a good sum of money"
+    },
+    {
+        id: 28,
+        emoji: "😶",
+        name: "No mouth",
+        meaning: "This conveys the situations when one has no words to speak due to being disappointment in a positive or negative way"
+    },
+    {
+        id: 29,
+        emoji: "😏",
+        name: "Smirk",
+        meaning: "The emoji to use when you’ve achieved something great and can’t wait to boast around it"
+    },
+    {
+        id: 30,
+        emoji: "😒",
+        name: "Unamused face",
+        meaning: "Generally related to negative emotions. This emoji conveys grumpiness or irritation towards someone or something"
+    },
+    {
+        id: 31,
+        emoji: "🙄",
+        name: "Face with rolling eyes",
+        meaning: "Used when the other person says something obvious or too dumb"
+    },
+    {
+        id: 32,
+        emoji: "😬",
+        name: "Grimacing face",
+        meaning: "The wide-open mouth and clenched teeth depict awkwardness or nervousness"
+    },
+    {
+        id: 33,
+        emoji: "🤥",
+        name: "Lying face",
+        meaning: "taken straight from the fictional character Pinocchio, this emoji depicts lying"
+    },
+    {
+        id: 34,
+        emoji: "😌",
+        name: "Relieved face",
+        meaning: "This emoji depicts a sense of calmness, peace or satisfaction"
+    },
+    {
+        id: 35,
+        emoji: "😔",
+        name: "Pensive face",
+        meaning: "An emoji used to describe a state of pain and sorrow"
+    },
+    {
+        id: 36,
+        emoji: "😴",
+        name: "Sleeping face",
+        meaning: "One can use this emoji to convey “good night” to a chat"
+    },
+    {
+        id: 37,
+        emoji: "😪",
+        name: "Sleepy face",
+        meaning: "The emoji depicts the feeling of exhaustion after a long tiring day"
+    },
+    {
+        id: 38,
+        emoji: "🤤",
+        name: "Drooling face",
+        meaning: "This emoji denotes the irresistibility over a yummy food"
+    },
+    {
+        id: 39,
+        emoji: "😷",
+        name: "Face with medical mask",
+        meaning: "This describes the wearing of a mask to protect from viruses and airborne diseases"
+    },
+    {
+        id: 40,
+        emoji: "🤒",
+        name: "Face with thermometer",
+        meaning: "This emoji depicts a sick person"
+    },
+    {
+        id: 41,
+        emoji: "🤕",
+        name: "Face with head bandage",
+        meaning: "The emoji denotes a bad head injury"
+    },
+    {
+        id: 42,
+        emoji: "🤢",
+        name: "Nauseated Face",
+        meaning: "The face depicting ‘about to throw up’"
+    },
+    {
+        id: 43,
+        emoji: "🤮",
+        name: "Face vomiting",
+        meaning: "This represents disgust or an actual food illness"
+    },
+    {
+        id: 44,
+        emoji: "🤧",
+        name: "Sneezing face",
+        meaning: "The emoji to use when someone has fallen sick and caught cold"
+    },
+    {
+        id: 45,
+        emoji: "🥵",
+        name: "Hot face",
+        meaning: "When the temperature is rising"
+    },
+    {
+        id: 46,
+        emoji: "🥶",
+        name: "Cold face",
+        meaning: "When temperature touches rock bottom. Extremely cold"
+    },
+    {
+        id: 47,
+        emoji: "🥴",
+        name: "Woozy face",
+        meaning: "When you had too much booze"
+    },
+    {
+        id: 48,
+        emoji: "😵",
+        name: "Face with crossed-out eyes",
+        meaning: "This emoji denotes disbelief or in some cases even death"
+    },
+    {
+        id: 49,
+        emoji: "🤯",
+        name: "Exploding head",
+        meaning: "Read, heard or saw something extremely shocking and amusing"
+    },
+    {
+        id: 50,
+        emoji: "🤠",
+        name: "Cowboy hat face",
+        meaning: "A simple smiley emoji with a cowboy hat"
+    },
+    {
+        id: 51,
+        emoji: "🥳",
+        name: "Partying face",
+        meaning: "Party time! Let’s party!"
+    },
+    {
+        id: 52,
+        emoji: "😎",
+        name: "Smiling face with sunglasses",
+        meaning: "Feeling overachieved and cool"
+    },
+    {
+        id: 53,
+        emoji: "🤓",
+        name: "Nerd face",
+        meaning: "Depicting a mind loaded with theoretical knowledge"
+    },
+    {
+        id: 54,
+        emoji: "🧐",
+        name: "Face with Monocle",
+        meaning: "This emoji depicts taking a closer into something"
+    },
+    {
+        id: 55,
+        emoji: "😕",
+        name: "Confused face",
+        meaning: "An emoji giving a look of uncertainty"
+    },
+    {
+        id: 56,
+        emoji: "☹️",
+        name: "Frowning face",
+        meaning: "This emoji denotes a deeply saddened state"
+    },
+    {
+        id: 57,
+        emoji: "😮",
+        name: "Face with Open mouth",
+        meaning: "A round mouth emoji describing a shock which makes one go ‘wow!’"
+    },
+    {
+        id: 58,
+        emoji: "😯",
+        name: "Hushed face",
+        meaning: "The emoji depicts mild amusement or surprise"
+    },
+    {
+        id: 59,
+        emoji: "😲",
+        name: "Astonished face",
+        meaning: "Used when one comes across something amusing or shocking in both positive or negative way"
+    },
+    {
+        id: 60,
+        emoji: "😳",
+        name: "Flushed face",
+        meaning: "A surprised emoji with eyes wide opened"
+    },
+    {
+        id: 61,
+        emoji: "🥺",
+        name: "Pleading face",
+        meaning: "Depicts wanting of something so bad that eyes become full of tears"
+    },
+    {
+        id: 62,
+        emoji: "😧",
+        name: "Anguished face",
+        meaning: "Depicting sadness and disappointment at the same time"
+    },
+    {
+        id: 63,
+        emoji: "😨",
+        name: "Fearful face",
+        meaning: "This emoji intends to depict fear from someone or something"
+    },
+    {
+        id: 64,
+        emoji: "😥",
+        name: "Sad but relieved face",
+        meaning: "Sad and relieved at the same time as things could’ve gone much worst"
+    },
+    {
+        id: 65,
+        emoji: "😭",
+        name: "Loudly crying face",
+        meaning: "This emoji conveys inconsolable grief but is also used in fun ways such as laughter or over the top joy"
+    },
+    {
+        id: 66,
+        emoji: "😱",
+        name: "Face screaming in fear",
+        meaning: "This emoji depicts a high degree of shock which can be both positive and negative"
+    },
+    {
+        id: 67,
+        emoji: "😖",
+        name: "Confounded face",
+        meaning: "The zig-zag mouth with crossed eyes on this emoji convey irritation, sadness and rather all kinds of negative feelings"
+    },
+    {
+        id: 68,
+        emoji: "😞",
+        name: "Disappointed face",
+        meaning: "This emoji conveys an unhappy and sad face due to some recent events"
+    },
+    {
+        id: 69,
+        emoji: "😓",
+        name: "Downcast face with sweat",
+        meaning: "This emoji conveys a certain degree of sadness, pain or disappointment"
+    },
+    {
+        id: 70,
+        emoji: "😫",
+        name: "Tired face",
+        meaning: "When you’ve had a rough and tiring day at work"
+    },
+    {
+        id: 71,
+        emoji: "🥱",
+        name: "Yawning face",
+        meaning: "Used when someone’s feeling sleepy"
+    },
+    {
+        id: 72,
+        emoji: "😤",
+        name: "Face with steam from nose",
+        meaning: "Taken inspiration from manga or anime, this emoji depicts angriness and frustration"
+    },
+    {
+        id: 73,
+        emoji: "🤬",
+        name: "Face with symbols on the mouth",
+        meaning: "This emoji depicts vulgar behaviour due to being too much furious at someone or something"
+    },
+    {
+        id: 74,
+        emoji: "👿",
+        name: "Angry face with horns",
+        meaning: "When the sender of this emoji becomes so angry that they turn into devils with horns"
+    },
+    {
+        id: 75,
+        emoji: "😈",
+        name: "Smiling face with horns",
+        meaning: "When the sender of this emoji is onto something mischievous or wicked"
+    },
+
+  ];
+  
+  export default emojipedia;
